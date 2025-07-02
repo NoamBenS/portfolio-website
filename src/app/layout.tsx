996d39fc09134fdb26b6299ed506ffe553bbebd2
Noam Ben Simon <noampvb@gmail.com>
2025-07-02 07:44:02 -0400

    Added basic component implementation. no data filled in yet.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <header className="fixed top-3 left-0 w-full z-50">
+          <div className="flex justify-center py-4">NOAM BEN SIMON</div>
+        </header>
         {children}
+        <footer className="fixed bottom-3 w-full z-50">
+          <div className="flex justify-center py-4 text-sm">
+            Website designed and coded by me.
+          </div>
+        </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function Home() {
   return (
-    <div>NOAM BEN SIMON</div>
+    <div className="flex flex-col justify-center items-center">
+      <div className="pt-15 text-center">
+        <Popover>
+          <PopoverTrigger className="hover:underline">CONTACT</PopoverTrigger>
+          <PopoverContent style={{backgroundColor: 'var(--background)'}}>
+
+          </PopoverContent>
+        </Popover>
+        <Tabs defaultValue="experience" className="w-1/2 pt-10">
+          <TabsList className="gap-4">
+            <TabsTrigger value="experience">EXPERIENCE</TabsTrigger>
+            <TabsTrigger value="projects">PROJECTS</TabsTrigger>
+            {/* <Separator orientation = "vertical" style={{backgroundColor: 'var(--foreground)'}}/> */}
+            <TabsTrigger value="skills">SKILLS</TabsTrigger>
+          </TabsList>
+          <TabsContent value="experience">experience.</TabsContent>
+          <TabsContent value="projects">projects.</TabsContent>
+          <TabsContent value="skills">skills.</TabsContent>
+        </Tabs>
+      </div>
+    </div>
   );
 }
