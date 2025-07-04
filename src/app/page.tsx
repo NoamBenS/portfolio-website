eadf634f6a895362538dcdb0def12d16ff2f10df
Noam Ben Simon <noampvb@gmail.com>
2025-07-03 23:34:50 -0400

    working on adding some pics and "what im up to" section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { contacts, projects, skills } from "@/lib/collections";
 
+import Image from "next/image";
+
 import {
   Popover,
   PopoverContent,
@@ -42,6 +44,14 @@ export default function Home() {
             ))}
           </PopoverContent>
         </Popover>
+        <div className="flex flex-col items-center">
+          <Image
+            src="/images/tel-aviv.jpg"
+            width={250}
+            height={200}
+            alt="tel-aviv"
+          />
+        </div>
         <Tabs
           defaultValue="overview"
           className="pt-10 flex flex-col items-center"
@@ -54,22 +64,22 @@ export default function Home() {
           {/* BREAKING UP THE CONTENT WITH A COMMENT*/}
           <TabsContent value="overview" className="w-1/4">
             <p>
-              Hey! I&apos;m Noam, an Honors Computer Science student at Yeshiva University.
+              Hey! I&apos;m Noam, an Honors Computer Science student at Yeshiva
+              University.
               <br />
               <br />
               While in school, I have worked hard to grow and develop my
               skillset and knowledge in the field, including projects such as
-              this website, a rhythm game I built with my friends, and a mock
-              of AWS Lambda for dsitributed compute.
+              this website, a rhythm game I built with my friends, and a mock of
+              AWS Lambda for dsitributed compute.
               <br />
               <br />
-              In my free time I like to read, go bouldering, cook,
-              and spend time with my friends (not all at once, though. I tried it once.
+              In my free time I like to read, go bouldering, cook, and spend
+              time with my friends (not all at once, though. I tried it once.
               BIG mistake. The food turned out ok, though).
               <br />
-              <br />
-              I hope you enjoy this website, and if you have any comments, critiques, or concerns,
-              please feel free to reach out to me!
+              <br />I hope you enjoy this website, and if you have any comments,
+              critiques, or concerns, please feel free to reach out to me!
             </p>
           </TabsContent>
           <TabsContent value="projects" className="max-w-1/4">
@@ -92,23 +102,45 @@ export default function Home() {
           <TabsContent value="skills" className="w-1/4">
             <Accordion type="multiple">
               {skills.map((array, index) => (
-                <AccordionItem key={index} value={arrays[index]} className="max-w-full">
-                <AccordionTrigger>{arrays[index]}</AccordionTrigger>
-                <AccordionContent className="flex flex-row flex-wrap justify-center">
-                {array.map((item, index) => (
-                  <div key={index}>
-                    <Badge>{item}</Badge>
-                  </div>
-                ))}
-                </AccordionContent>
-              </AccordionItem>
+                <AccordionItem
+                  key={index}
+                  value={arrays[index]}
+                  className="max-w-full"
+                >
+                  <AccordionTrigger>{arrays[index]}</AccordionTrigger>
+                  <AccordionContent className="flex flex-row flex-wrap justify-center">
+                    {array.map((item, index) => (
+                      <div key={index}>
+                        <Badge>{item}</Badge>
+                      </div>
+                    ))}
+                  </AccordionContent>
+                </AccordionItem>
               ))}
             </Accordion>
           </TabsContent>
         </Tabs>
       </div>
-      <Separator className="my-4 max-w-1/3" style={{ backgroundColor: "var(--foreground)" }} />
+      <Separator
+        className="my-4 max-w-1/3"
+        style={{ backgroundColor: "var(--foreground)" }}
+      />
       <h1 className="pt-10">WHAT I AM UP TO</h1>
+      <div className="pt-10 flex flex-row">
+        <div className="flex flex-col text-center">
+          <h1>PROFESSIONAL</h1>
+          <p>I AM WORKING</p>
+        </div>
+        <Separator
+        orientation="vertical"
+        className="mx-2 h-16"
+        style={{ backgroundColor: "var(--foreground)" }}
+        />
+        <div className="flex flex-col text-center">
+          <h1>PROFESSIONAL</h1>
+          <p>I AM WORKING</p>
+        </div>
+      </div>
     </div>
   );
 }
