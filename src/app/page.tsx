66f2d38247a5debffde49b5f0d8f90e9b547f534
Noam Ben Simon <noampvb@gmail.com>
2025-07-06 00:31:37 -0400

    changed formatting so now things look good across the board (mobile also)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ import {
 } from "@/components/ui/accordion";
 import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Home() {
   const arrays = ["LANGUAGES", "FRAMEWORKS", "APPS AND PROGRAMS"];
@@ -61,9 +62,8 @@ export default function Home() {
             <TabsTrigger value="projects">PROJECTS</TabsTrigger>
             <TabsTrigger value="skills">SKILLS</TabsTrigger>
           </TabsList>
-          {/* BREAKING UP THE CONTENT WITH A COMMENT*/}
-          <TabsContent value="overview" className="w-1/2">
-            <p>
+          <TabsContent value="overview" className="md:max-w-1/2 max-w-full">
+            <p className="p-3">
               Hey! I&apos;m Noam, an Honors Computer Science student at Yeshiva
               University.
               <br />
@@ -82,7 +82,7 @@ export default function Home() {
               critiques, or concerns, please feel free to reach out to me!
             </p>
           </TabsContent>
-          <TabsContent value="projects" className="max-w-1/2">
+          <TabsContent value="projects" className="max-w-2/3 md:w-1/2">
             <Carousel>
               <CarouselContent>
                 {projects.map((project, index) => (
@@ -99,13 +99,13 @@ export default function Home() {
               <CarouselNext />
             </Carousel>
           </TabsContent>
-          <TabsContent value="skills" className="w-1/4">
+          <TabsContent value="skills" className="w-full md:w-1/4">
             <Accordion type="multiple">
               {skills.map((array, index) => (
                 <AccordionItem
                   key={index}
                   value={arrays[index]}
-                  className="max-w-full"
+                  className="max-w-full p-3"
                 >
                   <AccordionTrigger>{arrays[index]}</AccordionTrigger>
                   <AccordionContent className="flex flex-row flex-wrap justify-center">
@@ -122,23 +122,67 @@ export default function Home() {
         </Tabs>
       </div>
       <Separator
-        className="my-4 max-w-1/3"
+        className="my-4 max-w-2/3 md:max-w-1/3"
         style={{ backgroundColor: "var(--foreground)" }}
       />
       <h1 className="pt-10">WHAT I AM UP TO</h1>
-      <div className="pt-10 flex flex-row">
+      <div className="pt-10 flex flex-row flex-wrap md:max-w-1/2 max-w-full justify-center gap-3">
         <div className="flex flex-col text-center">
-          <h1>PROFESSIONAL</h1>
-          <p>I AM WORKING</p>
+          <Card className="border-none shadow-none">
+            <CardContent>
+              <h1>PROFESSIONAL</h1>
+              <p>
+                I am currently interning for my university, building a website
+                to host educational content available to all. This work involves
+                designing and developing all aspects of the site, from the Figma
+                designs to the CMS schema and implementation. Although I am
+                still working in this position, I can already atest to the large
+                growth and experience I&apos;ve gained. This ranges from
+                learning to better manage large responsibilities and workloads
+                to expanding my knowledge in full-stack tools and frameworks.
+              </p>
+            </CardContent>
+          </Card>
         </div>
-        <Separator
-        orientation="vertical"
-        className="mx-2 h-16"
-        style={{ backgroundColor: "var(--foreground)" }}
-        />
         <div className="flex flex-col text-center">
-          <h1>PROFESSIONAL</h1>
-          <p>I AM WORKING</p>
+          <Card className="border-none shadow-none">
+            <CardContent>
+              <h1>ACADEMICS</h1>
+              <p>
+                I am entering my fourth and final year of a bachelors in
+                Computer Science with a focus on Distributed Systems. Through my
+                classes, coursework, projects, and extracurriculars, I have
+                built myself a strong foundation in the field of software
+                engineering. This includes work in parallel programming,
+                distributed systems, networking, and cybersecurity. Along with
+                this goes my work outside the classroom, such as in my position
+                as Head Website Manager for the YU Observer, an independent
+                school news publication, where my department works to manage and
+                update the publication&apos;s website.
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="flex flex-col text-center">
+          <Card className="border-none shadow-none">
+            <CardContent>
+              <h1>PERSONAL</h1>
+              <p>
+                I am currently enjoying my summer! Between my weekdays working,
+                I have found time on weekends to hang out with friends, visit
+                family - in Israel, Florida, and North Carolina, and tour New
+                York City! When I&apos;m not studying or working, you can find
+                me spending time reading, listening to music, or bouldering -
+                sometimes all three at once. If you&apos;re interested, here are
+                a couple books I&apos;ve recently read that I recommend: Words
+                of Radiance (Brandon Sanderson), Fahrenheit 451 (Ray Bradbury),
+                and The Hitchhiker&apos;s Guide to the Galaxy (Douglas Adams)
+                Here are some albums as well, if you are looking for
+                recommendations: Only God Was Above Us (Vampire Weekend), Tears
+                of Denial (Car Seat Headrest), The New Abnormal (The Strokes)
+              </p>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
